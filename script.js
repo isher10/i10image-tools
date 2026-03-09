fa7c4eecbb9369d4c3abffed5b32c9e88fa2b363
Isher education <ishwarbeniwal770@gmail.com>
2026-03-09 10:44:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -44,3 +44,45 @@ download.innerText = "Download Image";
 reader.readAsDataURL(file);
 
 }
+function convertToJPG() {
+
+const fileInput = document.getElementById("upload");
+const downloadLink = document.getElementById("download");
+
+if(fileInput.files.length === 0){
+alert("Please upload an image first");
+return;
+}
+
+const file = fileInput.files[0];
+const img = new Image();
+const reader = new FileReader();
+
+reader.onload = function(e){
+
+img.src = e.target.result;
+
+img.onload = function(){
+
+const canvas = document.createElement("canvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = img.width;
+canvas.height = img.height;
+
+ctx.drawImage(img,0,0);
+
+const jpg = canvas.toDataURL("image/jpeg",0.9);
+
+downloadLink.href = jpg;
+downloadLink.download = "converted-image.jpg";
+downloadLink.style.display = "inline-block";
+downloadLink.innerText = "Download JPG";
+
+}
+
+}
+
+reader.readAsDataURL(file);
+
+}
